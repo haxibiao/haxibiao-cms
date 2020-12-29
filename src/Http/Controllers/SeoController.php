75564f2e28d09024a4a4308294cddf2faed5ac31
ivan <ivan@mac>
2020-12-29 22:57:15 +0800

    更新收录查询

--- src/Http/Controllers/SeoController.php
+++ src/Http/Controllers/SeoController.php
@@ -8,15 +8,26 @@ class SeoController extends Controller
 {
     public function baiduInclude()
     {
-        $items = cache()->remember('neihan_sites_baidu_include_check', 60 * 24, function () {
+        $beian = cache()->remember('neihan_beian_baidu_include_check', 60 * 24, function () {
             $urls = "";
-            foreach (neihan_sites_domains() as $domain => $name) {
-                $urls .= "\nwww." . $domain;
+            foreach (neihan_beian_domains() as $domain => $name) {
+                // $urls .= "\nwww." . $domain;
                 $urls .= "\n" . $domain;
             }
             $urls = ltrim($urls);
             return baidu_include_check($urls);
         });
-        return view('seo.baidu_include')->with('items', $items);
+        $neihan = cache()->remember('movie_sites_baidu_include_check', 60 * 24, function () {
+            $urls = "";
+            foreach (neihan_sites_domains() as $domain => $name) {
+                // $urls .= "\nwww." . $domain;
+                $urls .= "\n" . $domain;
+            }
+            $urls = ltrim($urls);
+            return baidu_include_check($urls);
+        });
+        return view('seo.baidu_include')
+            ->with('beian', $beian)
+            ->with('neihan', $neihan);
     }
 }
